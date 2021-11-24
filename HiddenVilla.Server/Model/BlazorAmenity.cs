afa589e1adedc38f24a51349a88c878f4d96829a
Óscar Tutor Rubio <otutor@zimaltec.es>
2021-11-24 03:09:01 +0100

    initial commit

---
+++ HiddenVilla.Server/Model/BlazorAmenity.cs
@@ -0,0 +1,9 @@
+namespace HiddenVilla.Server.Model
+{
+    public class BlazorAmenity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description {get; set;}
+    }
+}
---
+++ HiddenVilla.Server/Model/BlazorRoomProp.cs
@@ -0,0 +1,10 @@
+
+namespace HiddenVilla.Server.Model
+{
+    public class BlazorRoomProp
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Value {get; set;}
+    }
+}
